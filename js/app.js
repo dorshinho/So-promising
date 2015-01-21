df3111aba9a1e2b4eae021599e84f02ddfc7807c
Dorsh <dorshdeans@gmail.com>
2015-01-21 18:09:43 -0500

    snapbacks and tattoos

--- js/app.js
+++ js/app.js
@@ -1,33 +1,41 @@
 window.onload = app;
 
 // runs when the DOM is loaded
-function app(){
+function app() {
     "use strict";
 
     // load some scripts (uses promises :D)
     loader.load(
         //css
-        {url: "./dist/style.css"},
+        {
+            url: "./dist/style.css"
+        },
         //js
-        {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"}
+        {
+            url: "./bower_components/jquery/dist/jquery.min.js"
+        }, {
+            url: "./bower_components/lodash/dist/lodash.min.js"
+        }
         // {url: "./bower_components/backbone/backbone.js"}
-    ).then(function(){
+    ).then(function() {
         document.querySelector("html").style.opacity = 1;
-  var token = "7c0555e739ceb57d4a4d07840111c9d533343f86"
+        var token = "7c0555e739ceb57d4a4d07840111c9d533343f86"
 
- var githubUsername = "dorshinho";
-        var url = "https://api.github.com/users/"+githubUsername+"?access_token="+token;
+        var githubUsername = "dorshinho";
+        var url = "https://api.github.com/users/" + githubUsername + "?access_token=" + token;
+        var left_col = document.querySelector(".col-1-3");
 
         $.get(url).then(drawProfile);
 
         function drawProfile(data) {
 
-            document.body.innerHTML = [
-    '<img src="', data.avatar_url,'"/>',
-    '<h2> blog:',data.blog,'</h2>',
-    '<h1>',data.login,'</h1>',
-    '<h2> blog:',data.blog,'</h2>',
+            left_col.innerHTML = [
+                '<img src="', data.avatar_url, '"/>',
+                '<h2>', data.name, '</h2>',
+                '<h6>', data.company, '</h6>',
+                '<h6>', data.blog, '</h6>',
+                '<h6>', data.location, '</h6>',
+                '<h6>', data.created_at, '</h6>',
 
             ].join('')
         }
@@ -46,4 +54,3 @@ function app(){
     // myRequest.send();
 
 }
-

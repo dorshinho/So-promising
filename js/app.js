02fde84fb919b78a24a229a17bfbb5d834fcebcc
Dorsh <dorshdeans@gmail.com>
2015-01-21 17:03:05 -0500

    getn' organized

--- js/app.js
+++ js/app.js
@@ -14,7 +14,36 @@ function app(){
         // {url: "./bower_components/backbone/backbone.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
-        // start app?
+  var token = "7c0555e739ceb57d4a4d07840111c9d533343f86"
+
+ var githubUsername = "dorshinho";
+        var url = "https://api.github.com/users/"+githubUsername+"?access_token="+token;
+
+        $.get(url).then(drawProfile);
+
+        function drawProfile(data) {
+
+            document.body.innerHTML = [
+    '<img src="', data.avatar_url,'"/>',
+    '<h2> blog:',data.blog,'</h2>',
+    '<h1>',data.login,'</h1>',
+    '<h2> blog:',data.blog,'</h2>',
+
+            ].join('')
+        }
+
     })
 
+
+
+    // var myRequest = new XMLHttpRequest();
+    // myRequest.onload = function(){
+    //     var json = JSON.parse(this.responseText);
+    //     console.log(json);
+    //     callback(json, this.statusMessage, this)
+    // };
+    // myRequest.open("get", "https://api.github.com/users/matthiasak", true);
+    // myRequest.send();
+
 }
+
